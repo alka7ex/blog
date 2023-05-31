22f3e7877e5941908df584ded654f8d27305f794
Farhienza Haikal <farhienzahaikal@gmail.com>
2023-06-01 04:24:27 +0700

    removing unccessary package adding google analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { Navbar } from '../components/Navbar'
 import { Footer } from '../components/Footer'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className='bg-base-200'>
+        <Script strategy="afterInteractive" src='https://www.googletagmanager.com/gtag/js?id=G-K9JFYBBM5Z'
+        />
+        <Script id='google-analyics-script' strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)};
+          gtag('js', new Date());
+           gtag('config', 'G-K9JFYBBM5Z');`}
+           </Script>        
         <Navbar />
         {children}
         <Footer />
